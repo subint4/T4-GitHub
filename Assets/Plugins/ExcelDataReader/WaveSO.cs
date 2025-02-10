d5e22974ff24d702c26c8558734a7aa526fe7c0d
T4subin <isvin56@naver.com>
2025-02-10 23:01:19 +0900

    02-10
    왜그럴까

---
+++ Assets/Plugins/ExcelDataReader/WaveSO.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Wave", menuName = "ScriptableObjects/WaveSO", order = 2)]
+public class WaveSO : ScriptableObject
+{
+    public int waveCount;
+    public float spawnRate;
+
+    // 이제 Unity에서 직렬화 가능
+    public SerializableDictionary<int, int> enemyCounts = new SerializableDictionary<int, int>();
+
+    public int GetTotalEnemies()
+    {
+        int totalEnemies = 0;
+        foreach (var enemy in enemyCounts)
+        {
+            totalEnemies += enemy.Value;
+        }
+        return totalEnemies;
+    }
+}
