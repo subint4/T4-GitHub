6bf661d6a2341d3790606f577fedb4629c34f402
T4subin <isvin56@naver.com>
2025-02-17 23:17:43 +0900

    02-17
    골드매니저 json인식하게 수정

--- Assets/Script/Manager/GoldManager.cs
+++ Assets/Script/Manager/GoldManager.cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.IO;
+using Newtonsoft.Json;
 
 public class GoldManager : MonoBehaviour
 {
     public static GoldManager Instance { get; private set; }
 
     [SerializeField] private TMP_Text goldText;
-    private int currentGold = 500;
+    private int currentGold;
+    private int gainGold;
+    private float gainInterval;
+    private Dictionary<int, GoldData> goldDataDictionary = new Dictionary<int, GoldData>();
+    private int currentStage = 1;
 
     public event Action<int> OnGoldChanged;
 
@@ -20,6 +28,61 @@ public class GoldManager : MonoBehaviour
         else
         {
             Destroy(gameObject);
+            return;
+        }
+        LoadGoldData();
+    }
+
+    private void Start()
+    {
+        SetStage(currentStage);
+        StartCoroutine(AutoGainGold());
+        UpdateGoldUI();
+    }
+
+    private void LoadGoldData()
+    {
+        string path = Path.Combine(Application.streamingAssetsPath, "GoldData.json");
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            var goldDataContainer = JsonConvert.DeserializeObject<GoldDataContainer>(json);
+            foreach (var data in goldDataContainer.Data)
+            {
+                goldDataDictionary[data.stagenum] = data;
+            }
+            Debug.Log("Gold 데이터 로드 완료.");
+        }
+        else
+        {
+            Debug.LogError("GoldData.json 파일을 찾을 수 없습니다!");
+        }
+    }
+
+    public void SetStage(int stage)
+    {
+        currentStage = stage;
+        if (goldDataDictionary.TryGetValue(stage, out GoldData data))
+        {
+            currentGold = data.startgold;
+            gainGold = data.gaingold;
+            gainInterval = data.sec;
+        }
+        else
+        {
+            currentGold = 500;
+            gainGold = 10;
+            gainInterval = 5;
+        }
+        UpdateGoldUI();
+    }
+
+    private IEnumerator AutoGainGold()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(gainInterval);
+            AddGold(gainGold);
         }
     }
 
@@ -51,7 +114,23 @@ public class GoldManager : MonoBehaviour
         OnGoldChanged?.Invoke(currentGold);
         if (goldText != null)
         {
-            goldText.text = $"{currentGold}";
+            goldText.text = currentGold.ToString();
         }
     }
+}
+
+[Serializable]
+public class GoldDataContainer
+{
+    public List<GoldData> Data;
+}
+
+[Serializable]
+public class GoldData
+{
+    public int id;
+    public int stagenum;
+    public int startgold;
+    public int sec;
+    public int gaingold;
 }
