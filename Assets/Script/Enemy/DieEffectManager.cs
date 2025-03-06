d1b8d426b5bf80be0628b949f6f87ab6ffaebc22
koqjh <94499117+koqjh@users.noreply.github.com>
2025-03-07 00:24:38 +0900

    2025-03-07 00 : 24
    Shop Unity z값 설정

---
+++ Assets/Script/Enemy/DieEffectManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieEffectManager : MonoBehaviour
+{
+    public GameObject deathEffect;
+    public GameObject blinkEffectPrefab;
+    public float effectDuration = 1.0f;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
