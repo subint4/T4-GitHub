f0c49aeef2ab983725a860b374afbda0969534de
subint4 <isvin56@naver.com>
2025-07-01 17:54:52 +0900

    hm?

--- Assets/Plugins/ExcelDataReader/WaveSO.cs
+++ Assets/Plugins/ExcelDataReader/WaveSO.cs
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 using System;
 using System.Collections.Generic;
 using UnityEngine;
@@ -29,39 +28,3 @@ public class WaveSO : ScriptableObject
     }
 
 }
-[Serializable]
-public class WaveSpawnData
-{
-    public int ID;
-    public int stagenum;
-    public int wave;
-    public int enemyID;
-    public int count;
-    public int SpawnGroup;
-    public float SpawnDelay;
-    public float interval;
-    public int SpawnLaneID;
-}
-=======
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Wave", menuName = "ScriptableObjects/WaveSO", order = 2)]
-public class WaveSO : ScriptableObject
-{
-    public int waveCount;
-    public float spawnRate;
-
-    // 이제 Unity에서 직렬화 가능
-    public SerializableDictionary<int, int> enemyCounts = new SerializableDictionary<int, int>();
-
-    public int GetTotalEnemies()
-    {
-        int totalEnemies = 0;
-        foreach (var enemy in enemyCounts)
-        {
-            totalEnemies += enemy.Value;
-        }
-        return totalEnemies;
-    }
-}
->>>>>>> Stashed changes
